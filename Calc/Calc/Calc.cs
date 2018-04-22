4d0c5abc959205b63603ff7e6f77ffa467a86c0c
nureevmarsel <mnurev@mail.ru>
2018-04-22 22:06:20 +0500

    proverka
    ghbdtn 'nj ghjdthrf

---
+++ Calc/Calc/Calc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Calc
+    {
+        Polynomial Pol1 = new Polynomial();
+        Polynomial Pol2 = new Polynomial();
+        public void Sum()
+        {
+            return (привет Лиза и Даня)
+        }
+    }
+}
